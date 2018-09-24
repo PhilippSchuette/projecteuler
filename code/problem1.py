b1c2337c1f5c5f276536139eedc8d26ac60ca7bf
Daniel Schuette <d.schuette@online.de>
2018-09-24 15:22:09 +0200

    add preliminary documentation to problem1.py

--- code/problem1.py
+++ code/problem1.py
@@ -1,5 +1,4 @@
-# template.py demonstrates how to structure the problem
-# solutions:
+# Project Euler Problem 1 Solution
 #
 # Problem statement:
 # If we list all the natural numbers below 10 that are multiples
@@ -7,7 +6,10 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 #
 # Solution description:
-# TODO: write description
+# This script compares a brute force algorithm with O(n) with a
+# formula-based algorithm with O(1)
+# Performance differences are best observed
+# with input numbers > 1000000
 #
 # Authors: Philipp Schuette, Daniel Schuette
 # Date: 2018/09/24
@@ -20,8 +22,9 @@ import numpy as np
 def slow_divisible(a=3, b=5, target=1000):
     """
     this function calculates the sum of all integers from 0 to
-    `target' that are divisible by `a' or `b' with a reminder
+    `target' that are divisible by `a' or `b' with a remander
     of 0
+    this is a slow implementation of the algorithm
     -------------------------
     params:
         a, b    - the integers that are used for the modulo operation
@@ -38,7 +41,13 @@ def slow_divisible(a=3, b=5, target=1000):
 
 def fast_summation(x, target):
     """
-
+    this function calculates the sum of all integers from 0 to
+    `target' that are divisible by `x' with a remander of 0
+    this is a fast implementation of the algorithm
+    -------------------------
+    params:
+        x       - the integer that is used for the modulo operation
+        target  - the interval over which to calculate the sum
     """
     n = (target - 1) // x
     sum = x * n * (n + 1) / 2
@@ -47,7 +56,9 @@ def fast_summation(x, target):
 
 def fast_divisible(a=3, b=5, target=1000):
     """
-
+    this function calculates the project euler problem 1
+    solution using a fast solution
+    `fast_summation()' is used to do so
     """
     sum = (fast_summation(a, target) + fast_summation(b, target) -
            fast_summation(a * b, target))
