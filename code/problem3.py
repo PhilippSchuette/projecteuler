0ba9bcca79e261045fc9ec2fa237e1605fc398ec
Daniel Schuette <d.schuette@online.de>
2018-09-30 21:17:12 +0200

    add problem 3 skeleton

---
+++ code/problem3.py
@@ -0,0 +1,12 @@
+# Project Euler Problem 2 Solution
+#
+# Problem statement:
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+#
+# Solution description:
+#
+# Author: Daniel Schuette
+# Date: 2018/09/30
+# License: MIT (see ../LICENSE.md)
