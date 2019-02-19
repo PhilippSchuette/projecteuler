0d5f48e8438b79f62f7129db4b701658cec60160
Philipp Schuette <43455001+PhilippSchuette@users.noreply.github.com>
2019-02-19 14:40:30 +0100

    Update template.c

---
+++ c_src/template.c
@@ -0,0 +1,15 @@
+/* template.c demonstrates how to structure
+the problem solutions: */
+
+/* Problem statement:
+
+*/
+/* Solution description:
+
+*/
+
+
+/* Author: <autor_name> */
+/* Date: 2019/../.. */
+/* License: MIT (see ../LICENSE.md) */
+
