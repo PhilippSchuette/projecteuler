267f2c45de541c591b9bc582231a8b151fb30ac1
DanielSchuette <d.schuette@online.de>
2019-03-03 18:04:19 +0100

    add improved problem 7 solution in Rust (20ms rt)

--- rust_src/src/lib/problem007.rs
+++ rust_src/src/lib/problem007.rs
@@ -29,9 +29,11 @@ pub fn is_prime(n: i64) -> bool {
         return false; /* n==2 must be handled first */
     }
 
-    // Determine primality via a O(n/2) approach (only odd
-    // numbers are tested, too).
-    for i in (3..(n / 2) + 1).step_by(2) {
+    // Determine primality via a O(sqrt(n)) approach. This
+    // method is valid because once sqrt(n) is reached, all
+    // larger divisors are simply dividends of a previous divisor.
+    // In addition, only odd numbers are tested.
+    for i in (3..(n as f64).sqrt() as i64 + 1).step_by(2) {
         if n % i == 0 {
             return false;
         }
