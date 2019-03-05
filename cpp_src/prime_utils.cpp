b80ff5c196421b0cbe7364f00ed8b2164d38e09b
tom <tom.praschan@gmail.com>
2019-03-05 10:50:11 +0100

    optimize problem 7 cpp runtime

--- cpp_src/prime_utils.cpp
+++ cpp_src/prime_utils.cpp
@@ -4,8 +4,10 @@
 // This function returns a boolean value indicating whether the input "n" is a primer number or not.
 bool is_prime(size_t n) {
     if (n == 0 or n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
 
-    for (size_t i = 2; i <= (size_t) sqrt((double) n); i++) {
+    for (size_t i = 3; i <= (size_t) sqrt((double) n); i++) {
         if (n % i == 0) {
             return false;
         }
